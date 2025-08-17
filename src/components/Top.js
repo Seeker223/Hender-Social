149c3898e905933ecd5c59a9af320ba670467763
BADUN OLUWATOBI <160877192+Seeker223@users.noreply.github.com>
2025-08-17 10:38:07 +0100

    Update Top.js

--- src/components/Top.js
+++ src/components/Top.js
@@ -17,7 +17,7 @@ const Top = () => {
             alt='logo' />
             </div>
             </Link>
-            <div className=' relative flex w-full h-[52px]'>
+            <div className='relative flex w-full h-[50px]'>
             <Circle />
             <Circle />
             <Circle />
@@ -32,6 +32,7 @@ const Top = () => {
 
 
 export default Top
+
 
 
 
