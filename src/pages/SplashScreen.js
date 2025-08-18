96c0b25340d789db08ecbc91ab304f3411a32372
BADUN OLUWATOBI <160877192+Seeker223@users.noreply.github.com>
2025-08-18 19:00:19 +0100

    Update SplashScreen.js

--- src/pages/SplashScreen.js
+++ src/pages/SplashScreen.js
@@ -12,8 +12,8 @@ const SplashScreen = () => {
               <div className=''>
                   <img
                     src={hender_xender}
-                    width={160}
-                    height={160}
+                    width={100}
+                    height={100}
                     alt='logo'
                   />
               </div>
@@ -26,6 +26,7 @@ const SplashScreen = () => {
 }
 
 export default SplashScreen
+
 
 
 
