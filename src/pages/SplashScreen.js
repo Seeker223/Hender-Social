52b25110ee10b4eda11203fef34c1d743e256cf3
BADUN OLUWATOBI <160877192+Seeker223@users.noreply.github.com>
2025-08-18 09:24:19 +0100

    Update SplashScreen.js removed background property

--- src/pages/SplashScreen.js
+++ src/pages/SplashScreen.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 
 const SplashScreen = () => {
   return (
-    <div className='body h-screen bg-white-500 md:bg-blue-500 lg:bg-green-500 bg-center flex justify-center items-center'>
+    <div className='body h-screen md:bg-blue-500 lg:bg-green-500 bg-center flex justify-center items-center'>
     <div className='h-screen flex flex-col justify-center items-center'>
             <Link to='/home'>
               <div className=''>
@@ -25,5 +25,6 @@ const SplashScreen = () => {
 }
 
 export default SplashScreen
+
 
 
